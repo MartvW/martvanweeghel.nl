543c1bbff459801dad37049d6d604fadaaa0cbf6
mweeghel <martvanweeghel@icloud.com>
2022-05-18 15:43:04 +0200

    Update

--- src/ts/components/primitives/layout/Layout.tsx
+++ src/ts/components/primitives/layout/Layout.tsx
@@ -1,4 +1,3 @@
-import Loading from '@components/primitives/loading/Loading';
 import { ReactChild, useState } from 'react';
 
 type Props = {
@@ -10,14 +9,11 @@ const Layout = ({ children }: Props) => {
 
     setTimeout(() => {
         setIsLoading(false);
-    }, 500);
+    }, 100);
 
     return (
         <main className='absolute top-0 bottom-0 w-full h-full theme bg-primary'>
-            <div className={`${isLoading ? 'opacity-100' : 'opacity-0'} ease-in-out duration-500`}>
-                <Loading />
-            </div>
-            <div className={`absolute top-0 left-0 ${isLoading ? 'opacity-100' : 'opacity-0'} w-full h-full bg-primary ease-in-out duration-500`}>
+            <div className={`absolute top-0 left-0 ${isLoading ? 'opacity-100 ' : 'opacity-0'} w-full h-full bg-primary ease-in-out duration-700`}>
             </div>
             {children}
         </main>
