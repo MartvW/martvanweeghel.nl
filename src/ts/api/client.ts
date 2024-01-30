e331027216ce3025ec2c0f93828e8272dfa9af1c
Mart van Weeghel <martvanweeghel@icloud.com>
2024-01-30 16:06:14 +0100

    Changed API url

--- src/ts/api/client.ts
+++ src/ts/api/client.ts
@@ -1,19 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 const defaultTimeoutLength = 4000; // 4 seconds
 
 const axiosInstance = axios.create({
-    baseURL: 'http://192.168.2.29:3005/martvanweeghel',
-    headers: {
-        'Accept': 'application/json',
-        'Cache-Control': 'no-cache',
-    }
+  baseURL: "https://api.martvanweeghel.nl/martvanweeghel",
+  headers: {
+    Accept: "application/json",
+    "Cache-Control": "no-cache",
+  },
 });
 
 export const executeGetRequest = (path: string) => {
-    const promise = axiosInstance.get(path, {
-        timeout: defaultTimeoutLength,
-    });
+  const promise = axiosInstance.get(path, {
+    timeout: defaultTimeoutLength,
+  });
 
-    return [promise];
+  return [promise];
 };
